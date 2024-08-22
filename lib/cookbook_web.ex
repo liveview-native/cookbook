ea56f556e44435c69870b4db441a62b8da8fd151
Brian Cardarella <bcardarella@gmail.com>
2024-08-22 17:21:13 -0400

    Make assets public

--- lib/cookbook_web.ex
+++ lib/cookbook_web.ex
@@ -17,7 +17,18 @@ defmodule CookbookWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(
+    assets
+    fonts
+    images
+    android-chrome-192x192.png
+    android-chrome-512x512.png
+    apple-touch-icon.png
+    favicon-16x16.png
+    favicon-32x32.png
+    robots.txt
+    site.webmanifest
+  )
 
   def router do
     quote do
